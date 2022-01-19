32775ac0bf087d16e64c5991926712a9b7bd5edb
saragrigolin <sara.grigolin95@gmail.com>
2022-01-19 12:38:36 +0100

    add searchbox

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,28 @@
 <template>
-  <header>header</header>
+  <header>
+		<div class="search">
+			<input id="search" v-model="inputText" type="text" name="search">
+			<button class="btn btn-primary" type="submit" @click="$emit('doSearch', inputText)"> Cerca</button>
+		</div>
+  </header>
 </template>
 
 <script>
 export default {
-
+	name: "Header",
+	props: {
+		inputValue: {
+			type: String,
+		}
+	},
+	data(){
+		return {
+			inputText: ''
+		}
+	}
 }
 </script>
 
-<style>
+<style lang="scss">
 
 </style>
