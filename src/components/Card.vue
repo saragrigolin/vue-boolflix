3778d8fd36a899d4b5f94e7a424d6e04939f748e
saragrigolin <sara.grigolin95@gmail.com>
2022-01-20 14:59:51 +0100

    create component with flags function

---
+++ src/components/Card.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="col">
+        <div class="item-card p-3">
+            <img :src="image" :alt="title">
+            <span>Title: {{title}}</span>
+            <span>Original title: {{original}}</span>
+            <span>Language: <i :class="'flag flag-' + getFlags(lang)"></i></span>
+            <span>Vote: {{vote}}</span>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Card",
+    props: [
+        'title',
+        'image',
+        'original',
+        'lang',
+        'vote',
+    ],
+    methods: {
+        getFlags(lang){
+            switch (lang){
+                case 'en':
+                    return 'us';
+                case 'ko':
+                    return 'kr';
+                case 'ja':
+                    return 'jp';
+                case 'ur':
+                    return 'pk';
+                case 'zh':
+                    return 'cn';
+                default:
+                    return lang;
+            }
+
+        }
+    }
+}
+
+</script>
+
+<style lang="scss">
+@import "../assets/scss/style.scss";
+.col {
+    padding: 0.5em 1em;
+    .item-card {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 100%;
+        border: 1px solid black;
+        img {
+            width: 100%;
+        }
+        span {
+            text-align: center;
+        }
+}
+}
+
+</style>
