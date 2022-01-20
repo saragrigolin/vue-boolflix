a78b7d77095607b439cc6ed950db06ababb7c635
saragrigolin <sara.grigolin95@gmail.com>
2022-01-20 12:57:26 +0100

    add movies e tv

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,23 @@
 <template>
 	<main>
-    	<ul>
-        	<li v-for="(movie, index) in inputHeader" :key="index">
-				<div>{{movie.title}}</div>
-				<div>{{movie.original_title}}</div>
-				<div>{{movie.original_language}}</div>
-				<div>{{movie.vote_average}}</div>
+    	<ul v-if="inputHeader.movies.length > 0">
+        	<li v-for="(movie, index) in inputHeader.movies" :key="index+'movies'">
+				<div>Title: {{movie.title}}</div>
+				<div>Original title: {{movie.original_title}}</div>
+				<div>Language: <i :class="(movie.original_language == 'en') ? 'flag flag-us' : `flag flag-${movie.original_language}`"></i></div>
+				<div>Vote: {{movie.vote_average}}</div>
 			</li>
-    	</ul>
+		</ul>
+		<h2 v-else>Nessun film trovato</h2>
+		<ul v-if="inputHeader.tv.length > 0">
+			<li v-for="(movie, index) in inputHeader.tv" :key="index+'tv'">
+				<div>Title: {{movie.name}}</div>
+				<div>Original title: {{movie.original_name}}</div>
+				<div>Language: <i :class="(movie.original_language == 'en') ? 'flag flag-us' : `flag flag-${movie.original_language}`"></i></div>
+				<div>Vote: {{movie.vote_average}}</div>
+			</li>
+		</ul>
+		<h2 v-else>Nessuna serie TV trovata</h2>
 	</main>
 </template>
 
@@ -17,6 +27,9 @@ export default {
 	name: "Main",
 	props: ["inputHeader"],
 	data(){
+		return {
+
+		}
 	}
 }
 </script>
